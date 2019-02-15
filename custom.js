75a7aa89151d858f5863b47616abbe368f635136
Jason Bazalar <jbazalar@kcet.org>
2019-02-15 10:40:08 -0800

    Removed uncessary tileLayer object variables

--- custom.js
+++ custom.js
@@ -99,10 +99,11 @@
 			
 			//// CARTO BASE MAPS - FREE TO USE ////
 			//// Max use 75,000 map impressions a Month per CartoDB, Inc.
+
 			//// MAP STYLE: Voyager
-			// let CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+			// L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
 			//// MAP STYLE: Voyager Labels Under
-			let CartoDB_VoyagerLabelsUnder = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png', {
+			L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png', {
 				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 				subdomains: 'abcd',
 				maxZoom: 20
